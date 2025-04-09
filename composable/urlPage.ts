562c3c2fd088e78ef6cfbecd7e1558ff3d1aaece
Alexandr <sasha-is-permi@mail.ru>
2025-04-09 18:20:03 +0500

    feature после установки nuxt и tailwind

---
+++ composable/urlPage.ts
@@ -0,0 +1,12 @@
+export const urlPage = async () => {
+    const route =  useRoute();
+    const routePath =  route.fullPath;
+    const host =  useRequestURL().host; 
+    const https = host !== "localhost:3000" ? "https://" : ""
+    return  https + host + routePath;;
+    }
+    
+    
+
+
+
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<!-- Главная страница -->
+<template>
+    <div>
+        Главная страница
+    </div>
+</template>
+<script setup lang="ts">
+import { urlPage } from "~/composable/urlPage";
+
+const url = await urlPage();
+
+useSeoMeta({
+    title: `Тестовое приложение по Nuxt`,
+    ogTitle: `Тестовое приложение по Nuxt`,
+    description: `Тестовое приложение по Nuxt.`,
+    ogDescription: `Тестовое приложение по Nuxt`,
+    keywords: ` `,
+    ogUrl: `${url}`,
+});
+
+</script>
+
